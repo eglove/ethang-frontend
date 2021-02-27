1140ca72b47a7519fcbe14c76361ccc4715fd40e
Ethan Glover <hello@ethang.email>
2021-02-27 16:54:20 -0600

    prodendpoint

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export const endpoint = `http://localhost:3000/api/graphql`;
-export const prodEndpoint = `fill me in when we deploy`;
+export const prodEndpoint = `https://ethang-backend.herokuapp.com/api/graphql`;
 export const perPage = 5;
