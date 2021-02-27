5aefcab34c3c0596b0a36b0589089142e3ffb65a
Ethan Glover <hello@ethang.email>
2021-02-26 21:18:55 -0600

    husky update

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types';
 import NProgress from 'nprogress';
+import { Router } from 'next/router';
 import Page from '../components/Page';
 import '../styles/nprogress.css';
-import { Router } from 'next/router';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
