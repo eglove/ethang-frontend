79795d0cf0061ddf775fb14d194c52c8f4220e4f
Ethan Glover <hello@ethang.email>
2021-02-26 22:06:02 -0600

    apollo config

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 import NProgress from 'nprogress';
 import { Router } from 'next/router';
+import { ApolloProvider } from '@apollo/client';
+import withData from '../lib/withData';
 import Page from '../components/Page';
 import '../styles/nprogress.css';
 
@@ -8,18 +10,30 @@ Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-function App({ Component, pageProps }) {
+function App({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+App.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
 
 App.propTypes = {
   Component: PropTypes.func,
   pageProps: PropTypes.object,
+  apollo: PropTypes.object,
 };
 
-export default App;
+export default withData(App);
