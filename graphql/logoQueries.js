bf90c7cb0bac3f5295de7408c77f86e91c28f283
Ethan Glover <hello@ethang.email>
2021-03-04 20:27:29 -0600

    one query const per page

--- graphql/logoQueries.js
+++ graphql/logoQueries.js
@@ -30,25 +30,3 @@ export const PORTFOLIO_LOGOS_QUERY = gql`
     alt
   }
 `;
-
-export const GITHUB_LOGO_QUERY = gql`
-  query GITHUB_LOGO_QUERY {
-    allLogos(where: { name: "GitHub" }) {
-      image {
-        publicUrlTransformed(transformation: { transformation: "SanitizeSVG" })
-      }
-      alt
-    }
-  }
-`;
-
-export const GLOBE_LOGO_QUERY = gql`
-  query GLOBE_LOGO_QUERY {
-    allLogos(where: { name: "Globe" }) {
-      image {
-        publicUrlTransformed(transformation: { transformation: "SanitizeSVG" })
-      }
-      alt
-    }
-  }
-`;
