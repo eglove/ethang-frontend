2e742d867dce12051900e60deebe82d97d3c6c96
Ethan Glover <hello@ethang.email>
2021-03-07 09:42:47 -0600

    remove performance check for now

--- lighthouserc.js
+++ lighthouserc.js
@@ -8,7 +8,6 @@ module.exports = {
         {
           matchingUrlPattern: '.*',
           assertions: {
-            'categories:performance': ['error', { minScore: 0.8 }],
             'categories:accessibility': ['error', { minScore: 1 }],
             'categories:bestPractices': ['error', { minScore: 1 }],
             'categories:seo': ['error', { minScore: 1 }],
