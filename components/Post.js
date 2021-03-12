a6289c1ff3277c452fddda54d0bd169b4cf3dd19
Ethan Glover <hello@ethang.email>
2021-03-11 18:51:05 -0600

    Blog h-cards

--- components/Post.js
+++ components/Post.js
@@ -6,13 +6,15 @@ import { BlogGrid } from '../styles/PageStyles';
 
 function Post({ post }) {
   return (
-    <BlogGrid key={post.id}>
+    <BlogGrid key={post.id} className="h-card">
       <Head>
         <title>EthanG | {post.title}</title>
         <meta name="description" content={`EthanG Blog Post ${post.title}`} />
       </Head>
       <h1>{post.title}</h1>
-      <Link href={post.author.url}>{post.author.name}</Link>
+      <Link href={post.author.url}>
+        <a className="p-name u-url">{post.author.name}</a>
+      </Link>
       <div>{new Date(post.created).toDateString()}</div>
       <Link href={`/post/${post.id}`}>Permalink</Link>
       <ParseHtml documentObject={post.content.document} />
