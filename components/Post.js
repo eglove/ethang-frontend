a7ef5f295c2e2f2cf946d57456d1066a88855a61
Ethan Glover <hello@ethang.email>
2021-03-11 18:58:45 -0600

    h-entry

--- components/Post.js
+++ components/Post.js
@@ -6,17 +6,21 @@ import { BlogGrid } from '../styles/PageStyles';
 
 function Post({ post }) {
   return (
-    <BlogGrid key={post.id} className="h-card">
+    <BlogGrid key={post.id} className="h-entry">
       <Head>
         <title>EthanG | {post.title}</title>
         <meta name="description" content={`EthanG Blog Post ${post.title}`} />
       </Head>
-      <h1>{post.title}</h1>
+      <h1 className="p-name">{post.title}</h1>
       <Link href={post.author.url}>
-        <a className="p-name u-url">{post.author.name}</a>
+        <a className="p-author h-card">{post.author.name}</a>
       </Link>
-      <div>{new Date(post.created).toDateString()}</div>
-      <Link href={`/post/${post.id}`}>Permalink</Link>
+      <div className="dt-published">
+        {new Date(post.created).toDateString()}
+      </div>
+      <Link href={`/post/${post.id}`}>
+        <a className="u-url">Permalink</a>
+      </Link>
       <ParseHtml documentObject={post.content.document} />
     </BlogGrid>
   );
